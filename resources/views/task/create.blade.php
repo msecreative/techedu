73db17db65661c750b3c6db5e9b6e855bcd2703d
msecreative <47583511+msecreative@users.noreply.github.com>
2022-02-03 09:36:12 +0600

    changes

--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -13,7 +13,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <form action="{{route('client.store')}}" method="POST" enctype="multipart/form-data">@csrf
+                    <form action="{{route('task.store')}}" method="POST" enctype="multipart/form-data">@csrf
                         <div class="mt-6 flex justify-between gap-4">
                             <div class="mt-6 flex-1">
                                 <label for="name" class="techedu-label">Name</label>
